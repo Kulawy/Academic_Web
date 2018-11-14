3ecaeedd84ee65a5b1286cc976ccffd6d22a4deb
jgeron <jgeron@pl.sii.eu>
2018-11-14 08:59:14 +0100

    Gra dziala przyzwoicie

--- for_beer_site/scriptGame.js
+++ for_beer_site/scriptGame.js
@@ -1,20 +1,23 @@
 
 //var buttonSend = document.getElementById("startGame");
 var contentWrapper = document.getElementById("wrapperID")
+var stateElement = document.getElementById("state")
+
 var drawnedCount = 0;
+var drinked = 0;
+var beerLimit;
 /*function startGameListener(){
     buttonSend.addEventListener("click", startGame, false);
 }*/
+// var buttonDrink;
 
-var buttonDring = document.getElementById("beerImg");
+// function drinkBeerListener(){
+//     buttonDrink.addEventListener("click", drinkBeer, false);
+// }
 
-function drinkBeerListener(){
-    buttonSend.addEventListener("click", drinkBeer, false);
-}
-
-function drinkBeer(){
-    buttonSend.setAttribute("src", "_img/emptyGlass.png");
-}
+// function drinkBeer(image){
+//     image.setAttribute("src", "_img/emptyGlass.png");
+// }
 
 function showAlert(){
     window.alert("look out you are drunk");
@@ -22,22 +25,25 @@ function showAlert(){
 
 function startGame(){
     clearPlain();
+    stateElement.innerHTML = "Sober"
+    drinked = 0;
     var beerCountString = window.prompt("After how many beers(500ml) you usualy get drunk?");
-    var beerLimit = parseInt(beerCountString);
+    beerLimit = parseInt(beerCountString);
     changeButtonSighn();
     drawnedCount = drawCount(beerLimit)
     for(var i=1; i <= drawnedCount; i++){
-            addBeerImage();
+            addBeerImage(i);
     }
 
+//    buttonDrink = document.getElementById("beerImg");
 }
 
 function drawCount(beers){
-    var countBeersToDrink = Math.floor( 1 + Math.random() * ( 4 * beers ) );
+    var countBeersToDrink = Math.floor( 1 + Math.random() * ( 5 * beers ) );
     return countBeersToDrink;
 }
 
-function addBeerImage(){
+function addBeerImage(i){
 
     /*
     var divBeerImg = document.createElement("div");
@@ -55,7 +61,13 @@ function addBeerImage(){
 
     var imageBeer = document.createElement("img");
     imageBeer.setAttribute("src", "_img/piwo.png")
-    imageBeer.setAttribute("class", "beerImg");
+    imageBeer.classList.add('beerImage');
+    imageBeer.setAttribute("id", "beerImg"+i);
+    imageBeer.addEventListener('click',function() {
+        imageBeer.setAttribute("src", "_img/emptyGlass.png");
+        drinked++;
+        checkState();
+    },false);
     contentWrapper.appendChild(imageBeer);
 
 
@@ -67,9 +79,33 @@ function clearPlain(){
     }
 }
 
+function checkState(){
+    if ( drinked >= 4*beerLimit ){
+        stateElement.innerHTML = "Zgon";
+    }
+    else if ( drinked >= 2*beerLimit ){
+        stateElement.innerHTML = "Wasted";
+    }
+    else if ( drinked >= beerLimit ){
+        stateElement.innerHTML = "Drunk";
+    }
+    //showPromptIfWantAnotherBeer();
+
+}
+
 function changeButtonSighn(){
     var startButton = document.getElementById("startGame")
     startButton.innerHTML = "Reset"
 }
+
+// function showPromptIfWantAnotherBeer() {
+//     if (drinked==drawnedCount) {
+//         var response=prompt('Another beer? y/n');
+//         if (response.toLowerCase()==y) {
+//             addBeerImage(++drawnedCount);
+//         }
+    
+//     }
+// }
 
 window.addEventListener("load", drinkBeerListener, false);
