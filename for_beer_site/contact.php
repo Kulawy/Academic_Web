d356620fff95e42c2dad0224b48156ddebf38f5a
cinek5 <marcinkrawczyk97@hotmail.com>
2018-12-11 23:11:41 +0100

    lista php1

--- for_beer_site/contact.php
+++ for_beer_site/contact.php
@@ -29,33 +29,52 @@
         </nav>
     </header>
     <section>
-        <form class="contactForm" method="POST" action="contact.php">
-            <label>email:</label>
-			<div>
-            <input name="address" id="address" />
-			</div>
-			   
-               <div><label>First name:</label></div>
-			   <div>
-               <input type = "text" name = "fname"></div>
-            <div><label>Last name:</label></div>
-			<div>
-              <input type = "text" name = "lname"></div>
-            <div><label>Email:</label></div>
-			  <div>
-              <input type = "text" name = "email"></div>            
-			  <div><label>Phone:</label></div>
-			  <div>
-              <input type = "text" name = "phone"
-                  placeholder = "(555) 555-5555"></div>
-              </div>  
-			  <div><label>Message:</label></div>
-			  <div>
-			  <textarea name="message" placeholder="enter your message here" ></textarea>
-			  </div>
-
-            <input id="sendMsg" type="submit" value="Send" />
-        </form>
+		<div style="text:center;">
+			<?php
+			
+			define('PRECEDES',"wczesniej");
+			define('FOLLOWS',"pozniej");
+			
+			$emailAddress=$_POST["address"];
+			if(!filter_var($emailAddress, FILTER_VALIDATE_EMAIL)) 
+			{
+				print( "<p> Podałeś nieprawidłowy adres emailowy, ale nic nie szkodzi. Nie dostaniesz darmowego piwka </p>");
+            }
+			
+			if (!preg_match( "/^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$/",$_POST["phone"]))                                  
+            {
+              print( "<p> Zły format numeru, prawidłowy to (555) 555-5555</p>                
+                  <p>Thank You.</p></body></html>" );
+               die();
+            }
+			$glupoty[0]="Hehe";
+			$glupoty[2]="gsdasdasd";
+			$glupoty[3]="glupotaasdsad3";
+			$glupoty[4]="glupota4";
+			foreach($glupoty as $value)
+			{
+				print( "<p>Kolejna głupota: $value </p>");
+			}
+			
+			$message = $_POST["message"];
+			print("<div> Wiadomosc od użytkownika: </div> <p style='border: 2px solid black'>$message</p>");
+	        $name=$_POST["fname"];
+			$last_name=$_POST["lname"];
+			$string_beg="<div>Imie $name jest ";
+			$string_mid="";
+			$string_end=" w alfabecie niż nazwisko $last_name </div>";
+			if ($name<$last_name)
+			{
+			    $string_mid=PRECEDES;
+			}
+			else 
+			{
+				$string_mid=FOLLOWS;
+			}
+		    print($string_beg.$string_mid.$string_end);
+	        
+			?>
+		</div>
     </section>
     <footer>
         <details>
