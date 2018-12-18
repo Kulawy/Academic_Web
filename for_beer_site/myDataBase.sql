54c185df63f6255c7ddfa5fa0bdbe9f90f24f66b
Kulawy <catatu1992@gmail.com>
2018-12-18 13:25:53 +0100

    poprawki

--- for_beer_site/myDataBase.sql
+++ for_beer_site/myDataBase.sql
@@ -1,11 +1,11 @@
 DROP DATABASE IF EXISTS site_users;
 
 CREATE DATABASE site_users;
-USE su;
+USE site_users;
 CREATE TABLE accounts
 (
    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   u_login VARCHAR(15) NOT NULL,
+   u_login VARCHAR(15) NOT NULL UNIQUE,
    u_password VARCHAR(15) NOT NULL,
    acces INT NOT NULL,
    u_name VARCHAR(21),
@@ -13,5 +13,7 @@ CREATE TABLE accounts
    u_email VARCHAR(30)
 );
 
-INSERT INTO accounts (ulogin, upassword) VALUES ('admin', 'admin', 3);
-INSERT INTO nazwa_tabeli (...) VALUES (...);
+INSERT INTO accounts (u_login, u_password, acces) VALUES ('admin', 'admin', 3);
+
+--TAMPLATE
+--INSERT INTO nazwa_tabeli (...) VALUES (...);
